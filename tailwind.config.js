37580ae6b867188fbf4df6274312c77aac791660
Hassanien <madienxz@gmail.com>
2022-03-24 17:05:48 +0200

    setup tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    colors: {
+      primary: "#01A6FF",
+
+      bg: {
+        light: "#F9FAFC",
+        dark: "#09152F",
+      },
+    },
+  },
+  plugins: [],
+};
